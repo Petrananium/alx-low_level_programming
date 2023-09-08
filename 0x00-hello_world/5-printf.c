26a97e793c533c03361117bb54619167b0006b66
petraanium <reasonesther@gmail.com>
2023-09-07 17:36:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - this is the printf
+ *
+ * Return: 0 when successful)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,/"):
+		return (0):
+}
