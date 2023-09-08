dce15993a788e613e04577e459f93d756c072b8f
petraanium <reasonesther@gmail.com>
2023-09-07 17:03:58 -0700

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: always 0 (success)
- *     */
+ * main - Entry pont
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-	        puts("\"Programming is like building a multilingual puzzle");
-		        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
