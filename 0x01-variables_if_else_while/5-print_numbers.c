47e791e1f59d750a31f0917d0e24768e0e530261
petraanium <reasonesther@gmail.com>
2023-09-11 05:23:40 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: always 0 ( success)
+ * main - Entry poin
+ * Description - prints number base 10	
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	int n = 0;
-	for (n = 0; n <= 9; a++)
+
+	if (n = 0; n <= 9; a++);
 	{ 
 		putchar(a + '0');
 	} 
