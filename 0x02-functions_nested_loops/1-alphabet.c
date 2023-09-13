4f9fff17c0583b8da11a941a8e31127376b5c3be
petraanium <reasonesther@gmail.com>
2023-09-12 17:43:34 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *print_alphabet - this is the function of the task
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+        {  
+           putchar(c);
+        } 
+         putchar('\n')
+
+} 
