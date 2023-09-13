9dce5c7f8e91b4482dd53f177f04e57acbc8bbc2
petraanium <reasonesther@gmail.com>
2023-09-12 18:36:23 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code
  *
@@ -6,14 +7,6 @@
  */
 int main(void)
 {
-	int r;
-
-
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+    jack_bauer();
+    return (0);
 }
-
