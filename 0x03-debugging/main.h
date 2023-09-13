9486feafdcdb713e96643603a0d3286532c5a446
petraanium <reasonesther@gmail.com>
2023-09-13 09:10:52 -0700

    0-main.c,

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,8 +3,6 @@
 
 #include <stdio.h>
 
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+void positive_or_negative(int i);
 
-
-#endif
+#endif /* MAIN_H */
