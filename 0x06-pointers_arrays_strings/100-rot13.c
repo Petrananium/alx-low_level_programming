ab582629838e43529543b8f1bad82e0dac3df606
petraanium <reasonesther@gmail.com>
2023-09-21 16:54:23 -0700

    100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * rot13 - main function
  *
@@ -7,21 +8,23 @@
  *
  * Return: String function
  */
+
 char *rot13(char *s)
 {
-int k, r;
-char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-for (k = 0; s[k] != '\0'; k++)
-{
-for (r = 0; r < 52; r++
-{
-if (s[k] == data1[r])
-{
-s[k] = datarot[r];
-break;
-}
-}
-}
-return (s);
+	int k, r;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+	for (r = 0; r < 52; r++)
+	{
+	if (s[k] == data1[r])
+	{
+	s[k] = datarot[r];
+	break;
+	}
+	}
+	}
+	return (s);
 }
